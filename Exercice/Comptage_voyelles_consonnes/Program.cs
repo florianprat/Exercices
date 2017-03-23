4cff4fba49d7c1c01521f2aa7edff8c61a5ca65a
fprat <fprat@FPRAT17-DE>
2017-03-23 17:38:00 +0100

    Ajout exercices comptage voyelles consonnes et login

---
+++ Exercice/Comptage_voyelles_consonnes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comptage_voyelles_consonnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int voyelle;
+            int consonne;
+
+            Console.WriteLine("Saisir un mot :");
+            string motSaisi = Console.ReadLine();
+
+            Comptage(motSaisi, out voyelle, out consonne);
+            Console.WriteLine("\"{0}\" comprend {1} consonnes et {2} voyelles", motSaisi, consonne, voyelle);
+            Console.ReadKey();
+        }
+
+        // Fonction de comptage des voyelles et des consonnes
+        static void Comptage(string motCompté, out int nbVoyelles, out int nbConsonnes)
+        {
+            nbVoyelles = 0;
+            nbConsonnes = 0;
+
+            for (int i = 0; i < motCompté.Length; i++)
+            {
+                if (motCompté[i] == 'a' || motCompté[i] == 'e' || motCompté[i] == 'i' || motCompté[i] == 'o' || motCompté[i] == 'u' || motCompté[i] == 'y')
+                {
+                    nbVoyelles++;
+                }
+            }
+            nbConsonnes = motCompté.Length - nbVoyelles;
+        }
+
+    }
+}
---
+++ Exercice/Login/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Demander à l'utilisateur de saisir un login
+            bool loginOK = false;
+
+            while (loginOK == false)
+            {
+                try
+                {
+                    Console.WriteLine("login :");
+                    string login = Console.ReadLine();
+                    VerifLogin(login);
+                    loginOK = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Le login doit comporter au moins 5 caractères");
+                    loginOK = false;
+                }
+            }
+
+            // Demander à l'utilisateur de saisir un mot de passe
+            bool mdpOK = false;
+
+            while (mdpOK == false)
+            {
+                try
+                {
+                    Console.WriteLine("mot de passe :");
+                    string mdp = Console.ReadLine();
+                    VerifMdp(mdp);
+                    mdpOK = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Le mot de passe doit comporter au moins 6 caractères, au plus 12, et ne doit pas commencer ni finir par un espace");
+                    mdpOK = false;
+                }
+            }
+
+            // Confirmation de la création du compte
+            Console.WriteLine("Votre compte a bien été créé. Un message vient de vous être envoyé.");
+
+            Console.ReadKey();
+        }
+
+        //
+        // Vérification du login
+        //
+        static void VerifLogin(string log)
+        {
+            if (log.Length < 5)
+            {
+                throw new FormatException("Le login doit comporter au moins 5 caractères");
+            }
+        }
+
+        //
+        // Vérification du mot de passe
+        //
+        static void VerifMdp(string mdpasse)
+        {
+            if (mdpasse.Length < 6 || mdpasse.Length > 12 || mdpasse[0] == ' ' || mdpasse[mdpasse.Length - 1] == ' ')
+            {
+                throw new FormatException("Le mot de passe doit comporter au moins 6 caractères, au plus 12, et ne doit pas commencer ni finir par un espace");
+            }
+        }
+
+    }
+}
