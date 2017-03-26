9049fd2f95980761ad560de7a6a425cabc2a9ca6
Florian Frat <fprat@isagri.fr>
2017-03-26 21:58:01 +0200

    Mise à jour de l'exercice Boîtes

--- Exercice/Boîtes/Program.cs
+++ Exercice/Boîtes/Program.cs
@@ -10,21 +10,36 @@ namespace Boîtes
     {
         static void Main(string[] args)
         {
+            Boite b1 = new Boite();
+
+            Console.WriteLine("Boîte de volume {0} cm3, de couleur {1} et en {2}", b1.CalculVolume, b1.EcritureCouleur, b1.LectureMatière);
+
+            Console.ReadKey();
         }
     }
 
-    public enum Couleurs {blanc, bleu, vert, jaune, orange, rouge, marron }
-    public enum Matière { carton, plastique, bois, métal}
+    public enum Couleur { blanc, bleu, vert, jaune, orange, rouge, marron }
+    public enum Matière { carton, plastique, bois, métal }
 
     public class Boite
     {
         #region Champs privés
-        private decimal _hauteur = 30;
-        private decimal _largeur = 30;
-        private decimal _longueur = 30;
-        private Couleurs _couleur;
-        private Matière _matière = Matière.carton;
-        
+        private decimal _hauteur;
+        private decimal _largeur;
+        private decimal _longueur;
+        private Couleur _couleur;
+        private Matière _matière;
+
+        #endregion
+
+        #region Constructeur
+        public Boite()
+        {
+            _hauteur = 30.0m;
+            _largeur = 30.0m;
+            _longueur = 30.0m;
+            _matière = Matière.carton;
+        }
         #endregion
 
         #region Propriétés
@@ -43,10 +58,15 @@ namespace Boîtes
             get { return _longueur; }
         }
 
-        public Couleurs EcritureCouleur
+        public Matière LectureMatière
+        {
+            get { return _matière; }
+        }
+
+        public Couleur EcritureCouleur
         {
             get { return _couleur; }
-            set { _couleur = value;  }
+            set { _couleur = value; }
         }
 
         public decimal CalculVolume
